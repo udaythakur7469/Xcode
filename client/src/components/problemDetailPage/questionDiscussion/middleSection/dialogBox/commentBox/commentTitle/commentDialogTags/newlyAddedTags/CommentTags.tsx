eca89021751009e9f00243ac74b4203c248c8dd0
Uday Thakur <udaythakur7469@gmail.com>
2025-09-21 18:29:12 +0530

    minimal css changes

--- client/src/components/problemDetailPage/questionDiscussion/middleSection/dialogBox/commentBox/commentTitle/commentDialogTags/newlyAddedTags/CommentTags.tsx
+++ client/src/components/problemDetailPage/questionDiscussion/middleSection/dialogBox/commentBox/commentTitle/commentDialogTags/newlyAddedTags/CommentTags.tsx
@@ -8,9 +8,9 @@ type CommentTagsProps = {
 
 const CommentTags: React.FC<CommentTagsProps> = ({ tag, onRemove }) => {
   return (
-    <div className="bg-blue-600 hover:bg-blue-700 flex flex-row items-center rounded-3xl px-3 py-1 mr-1 transition-colors">
+    <div className="bg-blue-600 hover:bg-blue-700 flex flex-row items-center cursor-default select-none rounded-3xl px-3 py-1 mr-1 transition-colors">
       <X
-        className="h-4 w-4 mr-2 font-bold cursor-pointer hover:bg-blue-800 rounded text-white"
+        className="h-4 w-4 mr-2 font-bold cursor-default rounded text-white"
         strokeWidth={3}
         onClick={onRemove}
       />
