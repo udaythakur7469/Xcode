d3eeb5f7a5048a7375f94f674c07e6891c73c60e
Uday Thakur <udaythakur7469@gmail.com>
2025-11-16 21:38:52 +0530

    minor error fixes

--- client/src/app/account/[name]/page.tsx
+++ client/src/app/account/[name]/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "@/components/landingPage/navbar/Navbar";
 import UserProfile from "@/components/accountPage/UserProfile";
+import { useAuthStore } from "@/features/authStore";
+import { useRouter } from "next/navigation";
 
 type pageProps = {
   params: {
@@ -10,9 +12,24 @@ type pageProps = {
   };
 };
 
-const page: React.FC<pageProps> = ({ params }) => {
+const Page: React.FC<pageProps> = ({ params }) => {
+  const router = useRouter();
+
+  const { isAuthenticated } = useAuthStore();
   const unwrappedParams = React.use(params);
   const { name } = unwrappedParams;
+
+  // 🔥 Redirect if not authenticated
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push("/");
+    }
+  }, [isAuthenticated, router]);
+
+  if (!isAuthenticated) {
+    return null; // avoid flashing content
+  }
+
   if (!name) {
     return <div className="text-red-500 text-xl">User not found</div>;
   }
@@ -24,4 +41,4 @@ const page: React.FC<pageProps> = ({ params }) => {
     </>
   );
 };
-export default page;
+export default Page;
