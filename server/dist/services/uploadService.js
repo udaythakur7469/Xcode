cc28c7433f6c27eda78494c77a96c286a64ea68f
Uday Thakur <udaythakur7469@gmail.com>
2026-01-26 02:15:23 +0530

    added cloudinary and multer for file upload and storage

---
+++ server/dist/services/uploadService.js
@@ -0,0 +1,29 @@
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+// Load environment variables
+dotenv.config();
+// Configure Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true,
+});
+export default cloudinary;
+export const storage = multer.memoryStorage();
+export const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024, // 5MB limit
+    },
+    fileFilter: (req, file, cb) => {
+        // Accept only image files
+        if (file.mimetype.startsWith("image/")) {
+            cb(null, true);
+        }
+        else {
+            cb(new Error("Only image files are allowed!"));
+        }
+    },
+});
