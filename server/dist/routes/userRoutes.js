bb5a9aa8c39b046094d035a3088e85ee20b4bc51
Uday Thakur <udaythakur7469@gmail.com>
2026-01-26 02:17:25 +0530

    added uploadProfilePicture route

--- server/dist/routes/userRoutes.js
+++ server/dist/routes/userRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { authenticatedUser, getUserHeatmapData, getUserSolvedLanguages, updateProfile, } from "../controllers/userController.js";
+import { authenticatedUser, getUserHeatmapData, getUserSolvedLanguages, updateProfile, updateProfilePicture, } from "../controllers/userController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { upload } from "../services/uploadService.js";
 const router = express.Router();
 router.route("/checkUser").get(authMiddleware, authenticatedUser);
 router.route("/profile").patch(authMiddleware, updateProfile);
+router
+    .route("/profile/picture")
+    .patch(authMiddleware, upload.single("picture"), updateProfilePicture);
 router.route("/userLanguages").get(authMiddleware, getUserSolvedLanguages);
 router.route("/heatmap").get(authMiddleware, getUserHeatmapData);
 export default router;
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -4,13 +4,18 @@ import {
   getUserHeatmapData,
   getUserSolvedLanguages,
   updateProfile,
+  updateProfilePicture,
 } from "../controllers/userController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { upload } from "../services/uploadService.js";
 
 const router = express.Router();
 
 router.route("/checkUser").get(authMiddleware, authenticatedUser);
 router.route("/profile").patch(authMiddleware, updateProfile);
+router
+  .route("/profile/picture")
+  .patch(authMiddleware, upload.single("picture"), updateProfilePicture);
 router.route("/userLanguages").get(authMiddleware, getUserSolvedLanguages);
 router.route("/heatmap").get(authMiddleware, getUserHeatmapData);
 
