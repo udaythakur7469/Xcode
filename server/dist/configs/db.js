0931c9130a810c3e2a0687114bd32f896a47485c
Uday Thakur <udaythakur7469@gmail.com>
2026-06-28 17:21:17 +0530

    feat(db): update PrismaClient logging to only capture errors

--- server/dist/configs/db.js
+++ server/dist/configs/db.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient({
-    log: ["query", "error"],
+    log: ["error"],
     errorFormat: "pretty",
 });
 export default prisma;
