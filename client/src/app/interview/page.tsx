b1ca2d7a91b198d9bffc1412a5c14e852e65ad0a
Uday Thakur <udaythakur7469@gmail.com>
2026-07-20 00:55:21 +0530

    feat(page): add metadata for interview page title

--- client/src/app/interview/page.tsx
+++ client/src/app/interview/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import InterviewLandingPage from "@/components/interviewPage/landingPage/InterviewLandingPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Mock Interviews | Xcode",
+};
 
 type pageProps = {};
 
