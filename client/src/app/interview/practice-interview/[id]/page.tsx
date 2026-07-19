77140d625a0387279372869100dfb58c6576623d
Uday Thakur <udaythakur7469@gmail.com>
2026-07-20 00:55:54 +0530

    feat(page): implement generateMetadata for dynamic page title

--- client/src/app/interview/practice-interview/[id]/page.tsx
+++ client/src/app/interview/practice-interview/[id]/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 import InterviewPractice from "@/components/interviewPage/interviewPracticePage/InterviewPractice";
+import { Metadata } from "next";
 
 type pageProps = { params: { id: number | null } };
+
+export async function generateMetadata({
+  params,
+}: pageProps): Promise<Metadata> {
+  const unwrappedParams = await params;
+  const { id } = unwrappedParams;
+  return {
+    title: id
+      ? `Interview Practice #${id} | Xcode`
+      : "Interview Practice | Xcode",
+  };
+}
 
 const page: React.FC<pageProps> = ({ params }) => {
   const unwrappedParams = React.use(params);
