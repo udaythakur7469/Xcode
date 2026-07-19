431b9a32eab35df07f8afccd254e68155ae5ad31
Uday Thakur <udaythakur7469@gmail.com>
2026-07-19 18:44:17 +0530

    feat(RevisionGuideDialog): add Revision Guide dialog component

---
+++ client/src/components/problemDetailPage/helperComponents/revisionGuide/RevisionGuideDialog.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React from "react";
+import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
+import RevisionGuideBox from "./RevisionGuideBox";
+
+type RevisionGuideDialogProps = { isOpen: boolean; onClose: () => void };
+
+const RevisionGuideDialog: React.FC<RevisionGuideDialogProps> = ({
+  isOpen,
+  onClose,
+}) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="max-w-6xl w-[100vw]">
+        <VisuallyHidden.Root>
+          <DialogTitle>Revision system guide</DialogTitle>
+        </VisuallyHidden.Root>
+        <div>
+          <RevisionGuideBox />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+export default RevisionGuideDialog;
