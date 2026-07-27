b42bd56d8f9438d757852cd0f2f96a7f3024fae2
Uday Thakur <udaythakur7469@gmail.com>
2026-07-27 11:46:50 +0530

    Refactor: add SupportedLanguage type and SUPPORTED_LANGUAGES data structure

---
+++ client/src/components/landingPage/landingPageData/languagesData.ts
@@ -0,0 +1,15 @@
+export type SupportedLanguage = {
+  name: string;
+  color: string;
+};
+
+export const SUPPORTED_LANGUAGES: SupportedLanguage[] = [
+  { name: "C++", color: "#f34b7d" },
+  { name: "Java", color: "#b07219" },
+  { name: "Python", color: "#3572A5" },
+  { name: "JavaScript", color: "#f1e05a" },
+  { name: "TypeScript", color: "#3178c6" },
+  { name: "Go", color: "#00ADD8" },
+  { name: "Rust", color: "#dea584" },
+  { name: "C", color: "#555555" },
+];
