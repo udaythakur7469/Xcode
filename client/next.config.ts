1ccee285bf49c55dbc229d026c1c49d02bffc588
Uday Thakur <udaythakur7469@gmail.com>
2026-07-28 12:17:14 +0530

    Refactor: update images configuration to use remotePatterns for improved flexibility

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["encrypted-tbn0.gstatic.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
   eslint: {
     ignoreDuringBuilds: true,
